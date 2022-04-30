49b5d32cc8f7e3beecac6d625207419c967f3ac5
Douglas Filipe <douglasfelipe.net77@gmail.com>
2022-04-29 23:32:45 -0400

    fix: Formating user.routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,6 +6,6 @@ import { LoginSchema, UserSchema } from "../schemas/user.schema";
 const route = Router()
 
 route.post("/register", validation(UserSchema), CreateUserController)
-route.post("/login",validation(LoginSchema), login)
+route.post("/login", validation(LoginSchema), login)
 
 export default route
